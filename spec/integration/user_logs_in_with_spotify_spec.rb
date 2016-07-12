969144915068a66166a0cd1793ff4027ac380a56
Colin Osborn <colin.osborn@gmail.com>
2016-07-12 00:47:20 -0600

    specs

--- spec/integration/user_logs_in_with_spotify_spec.rb
+++ spec/integration/user_logs_in_with_spotify_spec.rb
@@ -4,6 +4,10 @@ RSpec.feature "user logs in with spotify" do
   scenario "user needs to login" do
 
   visit root_path
-  expect(response).to have_status(200)
+  # expect(response).to have_status(200)
+  expect(response).to have_http_status(:success)
+
+  click_link "Sign in with Spotify"
+  expect(current_path).to eq("")
   end
 end
