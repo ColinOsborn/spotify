02dce2abf96b1fc294ba4cc39b4d416fae188330
Colin Osborn <colin.osborn@gmail.com>
2016-07-12 00:34:02 -0600

    more tests

--- spec/integration/user_logs_in_with_spotify_spec.rb
+++ spec/integration/user_logs_in_with_spotify_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "user logs in with spotify" do
+  scenario "user needs to login" do
+
   visit root_path
-  # expect()
+  expect(response).to have_status(200)
+  end
 end
