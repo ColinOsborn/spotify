d9c7f962fd353a72adf621887b0498d475645a05
Colin Osborn <colin.osborn@gmail.com>
2016-07-11 19:04:17 -0600

    setting keys
    adding keys, gems and oauth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionController < ApplicationController
+
+  def create
+    if user = User.from_omniauth(request.env["omniauth.auth"])
+      session[:user_id] = user.id
+    end
+    redirect_to root_path
+  end
+end
