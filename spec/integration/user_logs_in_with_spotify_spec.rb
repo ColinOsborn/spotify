b825ea9f303026a38d536149b1ca8f4549c1a468
Colin Osborn <colin.osborn@gmail.com>
2016-07-12 14:22:52 -0600

    adding tests

--- spec/integration/user_logs_in_with_spotify_spec.rb
+++ spec/integration/user_logs_in_with_spotify_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.feature "user logs in with spotify" do
   scenario "user needs to login" do
+    include Capybara::DSL
+    def setup
+      Capybara.app = Spotify::Application
+    end
 
   visit root_path
   # expect(response).to have_status(200)
   expect(response).to have_http_status(:success)
 
   click_link "Sign in with Spotify"
-  expect(current_path).to eq("")
+  expect(current_path).to eq("/")
   end
 end
